ce9e4e08f89ecfe4f35fb1e21bec89d5048576c6
genterist <genterist@live.com>
2016-10-16 05:12:00 +0000

    Basic scanner functions

--- main.c
+++ main.c
@@ -18,18 +18,9 @@
 #include <dirent.h>
 #include <errno.h>
 
-#include "buildTree.h"
-#include "traversals.h"
-
 int main(int argc, char *argv[])
 {
 	char *prog = argv[0]; //capture program name here
-	char theFileName[256];
-	char theName[256];
-
-	//init tree
-	Treeptr myBNTree;
-	myBNTree = NULL;
 
 	// if user put in many arguments
 	if (argc>2) {
@@ -40,40 +31,12 @@ int main(int argc, char *argv[])
 		// if a data file argument is specified
 		if (argv[1]!=NULL) {
 		    
-		    //construct file name
-		    strncpy(theFileName, argv[1], sizeof(theName));
-
-			FILE* filePtr = fopen (theFileName, "r");
-
-			//if that is a bogus argument or file is not readable
-			if (filePtr == NULL){
-				fprintf (stderr,"ERROR: file '%s' does not exist or not readable \n", argv[1]);
-			}
-			//if file is readable
-			else {
-				char i = "";
-				int nodes = 0;
-				int flag = 0; // value of 1 means raised flag
-
-				while (flag==0 && !feof (filePtr))
-				{
-					myBNTree= buildTree(myBNTree,(char) i, NULL);
-					nodes++;
-					if (fscanf (filePtr, "%c", &i) == 0){
-						fprintf (stderr,"ERROR: Invalid value read !! \n");
-						flag = 1;
-					}
-					fprintf (stderr,".");
-				}
-
-				fclose (filePtr);
-				fprintf (stderr,"\n %d character(s) read from %s \n", nodes, theFileName);
-			}
 
 		}
 		// if data file argument is not listed
 		// we will take input from keyboard or a redirection
 		else {
+		    char theName[256];
 			int i = 0;
 			int nodes = 0;
 			int flag = 0;  // value of 1 is raised
@@ -85,21 +48,14 @@ int main(int argc, char *argv[])
 				}
 				if (flag != 1)
 				{
-					myBNTree= buildTree(myBNTree,(int) i, NULL);
-					nodes++;
+//string recording here
+					nodes++;  //keep statistic of how many characters read
 				}
 			}
-			fprintf (stderr,"\n %d integer(s) were added to the tree. \n", nodes);
+			fprintf (stderr,"\n %d character(s) were read. \n", nodes);
 
 		}
 
-		//Traverse functions :: output to screen if no output file name specified
-		sprintf(theFileName, "%s.inorder", theName);
-		TravInOrder(myBNTree, 0, theFileName);
-		sprintf(theFileName, "%s.preorder", theName);
-		TravPreOrder(myBNTree, 0, theFileName);
-		sprintf(theFileName,"%s.postorder", theName);
-		TravPostOrder(myBNTree, 0, theFileName);
 	}
 	return EXIT_SUCCESS;
 }
